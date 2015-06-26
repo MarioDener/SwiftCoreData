373a374bdbd4f2fb9a2eb64ba3c58d5f214beea5
Mario Sanchez <MarioJose1693@gmail.com>
2015-06-25 23:09:57 -0600

    1er commit con la base del ejercicio Core Data.

--- Core Data Demo/ViewController.swift
+++ Core Data Demo/ViewController.swift
@@ -7,12 +7,67 @@
 //
 
 import UIKit
-
+import CoreData
 class ViewController: UIViewController {
 
+    @IBOutlet weak var name: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        var appDel:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        var context:NSManagedObjectContext = appDel.managedObjectContext!
+        var newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context) as! NSManagedObject
+        
+//        SECCION DONDE SE REALIZA EL REGISTRO
+        
+//        newUser.setValue("Mario Jose", forKey: "username")
+//        newUser.setValue("Pass1", forKey: "password")
+//        context.save(nil)
+        
+//        FIN DE SECCION DONDE SE REALIZA REGISTRO
+        
+        
+        
+//        SECCION PARA IMPRIMIR EN PANTALL LOS REGISTROS
+        
+        var request  = NSFetchRequest(entityName: "Users")
+        request.returnsObjectsAsFaults = false
+        request.predicate = NSPredicate(format: "username = %@", "Mario")
+        var result = context.executeFetchRequest(request, error: nil)
+        
+
+        if result?.count > 0 {
+            println(result)
+            for result: AnyObject in result!{
+                if let user = result.valueForKey("username") as? String{
+//                    print(user)
+                    //        SECCION DONDE SE ELIMINARA UN REGISTRO
+                        if user == "Mario"
+                        {
+                            
+//                            context.deleteObject(result as! NSManagedObject)
+//                            print(user + " has been delete")
+                            result.setValue("pass6", forKey: "password")
+                        }
+                    //        FIN DE LA ELIMNACION DEL REGISTRO
+                }
+                context.save(nil)
+            }
+        }
+        else
+        {
+            print("No results")
+        }
+//        FINAL DE LA IMPRESION EN PATALLA
+        
+        
+
+        
+        
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +77,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
